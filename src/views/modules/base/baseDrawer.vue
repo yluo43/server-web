0771c1fb4c4f530b45e4651c7fc069ec374a83bd
lichengjin <lichengjin@ebupt.com>
2024-02-04 14:49:58 +0800

    [Feat]: 不要确认信息吧

--- src/views/modules/base/baseDrawer.vue
+++ src/views/modules/base/baseDrawer.vue
@@ -19,11 +19,7 @@ export default {
     handleClose: {
       type: Function,
       default: function (done) {
-        this.$confirm('确认关闭？')
-          .then((_) => {
-            done()
-          })
-          .catch((_) => {})
+        done()
       }
     }
   },
