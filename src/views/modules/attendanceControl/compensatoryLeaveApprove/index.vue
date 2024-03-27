7e7b4ba92e847ff1b04ce6e864c81361358a15b5
guoyangdong <guoyangdong@e-byte.com>
2024-03-27 11:19:30 +0800

    新建考勤管控目录

---
+++ src/views/modules/attendanceControl/compensatoryLeaveApprove/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div style="height: 100%"></div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {},
+  data() {
+    return {}
+  },
+
+  mounted() {},
+  created() {},
+  methods: {}
+}
+</script>
+
+<style lang="scss" scoped></style>
