744ab2ce07e801a7c2c42f809ac08011efc7d965
guoyangdong <guoyangdong@e-byte.com>
2024-03-29 15:20:11 +0800

    [feat]:加班调休审批页面

--- src/views/modules/attendanceControl/compensatoryLeaveApplication/index.vue
+++ src/views/modules/attendanceControl/compensatoryLeaveApplication/index.vue
@@ -41,7 +41,13 @@
           </el-col>
           <el-col :span="6">
             <el-form-item label="加班开始时间" prop="overTimeStartTime">
-              <el-date-picker v-model="overtimeDataForm.overTimeStartTime" type="datetime" placeholder="选择日期时间"></el-date-picker>
+              <el-date-picker
+                v-model="overtimeDataForm.overTimeStartTime"
+                value-format="yyyy-MM-dd HH:mm"
+                format="yyyy-MM-dd HH:mm"
+                type="datetime"
+                placeholder="选择日期时间"
+              ></el-date-picker>
             </el-form-item>
           </el-col>
           <el-col :span="6">
@@ -65,7 +71,13 @@
         <el-row :gutter="20">
           <el-col :span="9">
             <el-form-item label="调休开始时间" prop="compensatoryLeaveStartTime">
-              <el-date-picker v-model="compensatoryLeaveDataForm.startDate" type="date" placeholder="选择日期"></el-date-picker>
+              <el-date-picker
+                v-model="compensatoryLeaveDataForm.startDate"
+                value-format="yyyy-MM-dd"
+                format="yyyy-MM-dd"
+                type="date"
+                placeholder="选择日期"
+              ></el-date-picker>
               <el-time-select
                 v-model="compensatoryLeaveDataForm.startTime"
                 :picker-options="{
@@ -79,20 +91,26 @@
           </el-col>
           <el-col :span="9">
             <el-form-item label="调休结束时间" prop="compensatoryLeaveEndTime">
-              <el-date-picker v-model="value1" type="date" placeholder="选择日期"></el-date-picker>
+              <el-date-picker
+                v-model="compensatoryLeaveDataForm.endDate"
+                value-format="yyyy-MM-dd"
+                format="yyyy-MM-dd"
+                type="date"
+                placeholder="选择日期"
+              ></el-date-picker>
               <el-time-select
-                v-model="value"
+                v-model="compensatoryLeaveDataForm.endTime"
                 :picker-options="{
-                  start: '09:00',
-                  step: '03:00',
-                  end: '12:00'
+                  start: '12:00',
+                  step: '06:00',
+                  end: '18:00'
                 }"
                 placeholder="选择时间"
               ></el-time-select>
             </el-form-item>
           </el-col>
           <el-col :span="6">
-            <el-form-item label="调休天数" prop="projectManager">{{ overtimeDataForm.projectManager }}天</el-form-item>
+            <el-form-item label="调休天数" prop="days">{{ compensatoryLeaveDataForm.days }}天</el-form-item>
           </el-col>
         </el-row>
         <div style="margin-top: 20px">
@@ -109,6 +127,20 @@ export default {
   components: {},
   props: {},
   data() {
+    const validateCompensatoryLeaveStartTime = (rule, value, callback) => {
+      if (!value || !this.compensatoryLeaveDataForm.startDate || !this.compensatoryLeaveDataForm.startTime) {
+        callback(new Error('请选择调休开始时间'))
+        return
+      }
+      callback()
+    }
+    const validateCompensatoryLeaveEndTime = (rule, value, callback) => {
+      if (!value || !this.compensatoryLeaveDataForm.endDate || !this.compensatoryLeaveDataForm.endTime) {
+        callback(new Error('请选择调休结束时间'))
+        return
+      }
+      callback()
+    }
     return {
       overtimeProjects: [],
       overtimeDataForm: {
@@ -119,7 +151,7 @@ export default {
         homeFlag: '',
         overTimeStartTime: '',
         overTimeEndTime: '',
-        overtimeDuration: ''
+        overtimeDuration: 0
       },
       compensatoryLeaveDataForm: {
         compensatoryLeaveStartTime: '',
@@ -127,7 +159,8 @@ export default {
         startDate: '',
         endDate: '',
         startTime: '',
-        endTime: ''
+        endTime: '',
+        days: 0
       },
       overtimeRules: {
         overtimeProject: [{ required: true, message: '请选择加班项目', trigger: 'change' }],
@@ -136,7 +169,8 @@ export default {
         overTimeEndTime: [{ required: true, message: '请选择加班结束时间', trigger: 'change' }]
       },
       compensatoryLeaveRules: {
-        compensatoryLeaveStartTime: [{ required: true, message: '请选择调休开始时间', trigger: 'change' }]
+        compensatoryLeaveStartTime: [{ required: true, validator: validateCompensatoryLeaveStartTime, trigger: 'change' }],
+        compensatoryLeaveEndTime: [{ required: true, validator: validateCompensatoryLeaveEndTime, trigger: 'change' }]
       }
     }
   },
@@ -147,27 +181,48 @@ export default {
     'compensatoryLeaveDataForm.startDate': {
       handler(value) {
         if (value) {
-          this.compensatoryLeaveDataForm.compensatoryLeaveStartTime = value + this.compensatoryLeaveDataForm.startTime
+          this.compensatoryLeaveDataForm.compensatoryLeaveStartTime = value + '\xa0' + this.compensatoryLeaveDataForm.startTime
         }
       }
     },
     'compensatoryLeaveDataForm.startTime': {
       handler(value) {
         if (value) {
-          this.compensatoryLeaveDataForm.compensatoryLeaveStartTime = this.compensatoryLeaveDataForm.startDate + value
+          this.compensatoryLeaveDataForm.compensatoryLeaveStartTime = this.compensatoryLeaveDataForm.startDate + '\xa0' + value
         }
       }
     },
+
     overtimeDuration(value) {
       if (value) {
         this.overtimeDataForm.overtimeDuration = value
       }
+    },
+    compensatoryLeaveEndTime(value) {
+      if (value) {
+        this.compensatoryLeaveDataForm.compensatoryLeaveEndTime = value
+      }
+    },
+    days(value) {
+      this.compensatoryLeaveDataForm.days = value
     }
   },
   computed: {
     overtimeDuration() {
       if (this.overtimeDataForm.overTimeStartTime && this.overtimeDataForm.overTimeEndTime) {
         return (new Date(this.overtimeDataForm.overTimeEndTime).getTime() - new Date(this.overtimeDataForm.overTimeStartTime).getTime()) / 1000 / 60 / 60
+      } else {
+        return 0
+      }
+    },
+    compensatoryLeaveEndTime() {
+      return this.compensatoryLeaveDataForm.endDate + '\xa0' + this.compensatoryLeaveDataForm.endTime
+    },
+    days() {
+      let diffDay1 = this.getDaysBetweenDates(new Date(this.compensatoryLeaveDataForm.endDate), new Date(this.compensatoryLeaveDataForm.startDate))
+      let diffDay2 = this.getTimeDays()
+      if (diffDay1 || diffDay2) {
+        return diffDay1 + diffDay2
       } else {
         return 0
       }
@@ -225,7 +280,27 @@ export default {
     },
     //加班申请取消
     cancelOverTime() {},
-
+    //计算两个日期之间的天数
+    getDaysBetweenDates(date1, date2) {
+      var oneDay = 24 * 60 * 60 * 1000 // 一天的毫秒数
+      var timeDiff = Math.abs(date2.getTime() - date1.getTime()) // 两个日期的差值的毫秒数
+      var diffDays = Math.floor(timeDiff / oneDay) // 差值的天数
+      return diffDays
+    },
+    //计算两个时间之间的天数
+    getTimeDays() {
+      let diffDay
+      if (this.compensatoryLeaveDataForm.startTime == '09:00' && this.compensatoryLeaveDataForm.endTime == '12:00') {
+        diffDay = 0.5
+      } else if (this.compensatoryLeaveDataForm.startTime == '09:00' && this.compensatoryLeaveDataForm.endTime == '18:00') {
+        diffDay = 1
+      } else if (this.compensatoryLeaveDataForm.startTime == '12:00' && this.compensatoryLeaveDataForm.endTime == '12:00') {
+        diffDay = 0
+      } else if (this.compensatoryLeaveDataForm.startTime == '12:00' && this.compensatoryLeaveDataForm.endTime == '18:00') {
+        diffDay = 0.5
+      }
+      return diffDay
+    },
     //日期格式转换
     formatDate(date) {
       var date = new Date(date)
